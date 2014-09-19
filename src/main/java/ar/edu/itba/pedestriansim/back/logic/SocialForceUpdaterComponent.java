8e0387803b53a4ed56e8c6f6c05c8e3fb69a1fba
Gonzalo Castiglione <gcastiglione@bdev.com.ar>
2014-09-18 21:13:15 -0300

    subo social force component.

---
+++ src/main/java/ar/edu/itba/pedestriansim/back/logic/SocialForceUpdaterComponent.java
@@ -0,0 +1,89 @@
+package ar.edu.itba.pedestriansim.back.logic;
+
+import org.newdawn.slick.geom.Line;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Vector2f;
+
+import ar.edu.itba.pedestriansim.back.entity.Pedestrian;
+import ar.edu.itba.pedestriansim.back.entity.PedestrianArea;
+import ar.edu.itba.pedestriansim.back.entity.force.DrivingForce;
+import ar.edu.itba.pedestriansim.back.entity.force.SpringForceModel;
+import ar.edu.itba.pedestriansim.back.entity.physics.Vectors;
+
+public class SocialForceUpdaterComponent extends PedestrianAreaStep {
+
+	private static final float kn = 0.5f;
+	private static final float kt = 0;
+	private static final float A = 600;
+	private static final float B = 0.5f;
+
+	private static final SpringForceModel wallCollisionForceModel = new SpringForceModel();
+	private static final DrivingForce drivingForce = new DrivingForce();
+
+	@Override
+	public void update(PedestrianArea input) {
+		for (Pedestrian pedesitan : input.pedestrians()) {
+			pedesitan.getFuture().getBody().setCenter(pedesitan.getBody().getCenter());
+			Vector2f force = pedesitan.getBody().getAppliedForce();
+			force.set(Vectors.zero());
+			Vector2f fSocial = fSocial(pedesitan, input.pedestrians());
+			Vector2f fCollision = fCollision(pedesitan, input.obstacles());
+			Vector2f fDesire = fDesire(pedesitan);
+			force.add(fSocial).add(fCollision).add(fDesire);
+		}
+	}
+
+	private Vector2f fSocial(Pedestrian pi, Iterable<Pedestrian> others) {
+		Vector2f force = new Vector2f();
+		Vector2f center = pi.getBody().getCenter();
+		float ri = pi.getBody().getRadius();
+		for (Pedestrian pj : others) {
+			if (pi == pj) {
+				continue;
+			}
+			float rj = pj.getBody().getRadius();
+			Vector2f rij = pj.getBody().getCenter().copy().sub(center);
+			float d = rij.length();
+			Vector2f en = rij.copy().normalise();
+			Vector2f et = new Vector2f(en.y, -en.x);
+			float eps = d - (ri + rj);
+			if (eps < 0) {
+				// Contact force
+				Vector2f fContactn = en.copy().scale(-eps * kn);
+				float vt = pi.getBody().getVelocity().dot(et);
+				// TODO: esta bien usar la velocidad?
+				Vector2f fContactt = et.copy().scale(vt * eps * kt);
+				fContactn.add(fContactt).scale(g(eps));
+				force.add(fContactn);
+			} else {
+				// Social force
+				float exp = (float) Math.exp(eps / B);
+				Vector2f fSocial = en.copy().scale(exp * A);
+				force.add(fSocial);
+			}
+			force.add(new Vector2f());
+		}
+		return force;
+	}
+
+	private float g(float eps) {
+		return eps;
+	}
+
+	private Vector2f fCollision(Pedestrian pedesitan, Iterable<Shape> shapes) {
+		Vector2f f = new Vector2f();
+		for (Shape obstacle : shapes) {
+			Line line = (Line) obstacle;
+			if (pedesitan.getShape().intersects(line)) {
+				f.add(wallCollisionForceModel.getForce(pedesitan.getBody().getCollitionShape(), line));
+			}
+		}
+		return f;
+	}
+
+	private Vector2f fDesire(Pedestrian p1) {
+		Vector2f center = p1.getBody().getCenter();
+		Vector2f target = p1.getTargetSelection().getTarget().getClosesPoint(center);
+		return drivingForce.getForce(p1.getBody(), target, p1.getMaxVelocity());
+	}
+}
